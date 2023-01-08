815387f0e6a20af65eece7949978ed085cf24dd3
fregnani123 <fabianofregnani@hotmail.com>
2023-01-08 10:00:01 -0300

    Estudando sunday

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 
 
 
+let data = new Date();
+let day = data.getDay();
+const month = data.getMonth();
+let years = data.getFullYear();
+let monthDay = data.getDate();
 
-function sortear(){
-    let nx = 50;
-    let ns = Math.round(Math.random() * nx)
 
- document.querySelector("#res").innerHTML= ns
+function diaDoMes(){
+  let diaMes = []
 
 }
 
+function diaDaSemana(){
+
+let diaSemana = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+if (day === 0 ) return diaSemana[0]
+if (day === 1 ) return diaSemana[1]}
+
+function mostrarNatela(){
+ let dataFormatada = diaDaSemana()
+
+ let resposta = document.querySelector("#res");
+ resposta.innerHTML = `${dataFormatada}, ${monthDay} de`}
+
+mostrarNatela()
