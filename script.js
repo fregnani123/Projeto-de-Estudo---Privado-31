817a891ff40ca9b42bce5361fc476f5272761940
fregnani123 <fabianofregnani@hotmail.com>
2023-01-09 09:45:59 -0300

    atualizar estudos

--- script.js
+++ script.js
@@ -3,27 +3,67 @@
 
 let data = new Date();
 let day = data.getDay();
-const month = data.getMonth();
+let monthYaer = data.getMonth();
 let years = data.getFullYear();
 let monthDay = data.getDate();
+console.log(monthDay);
 
+function diaDoMes() {
 
-function diaDoMes(){
-  let diaMes = []
+    monthYaersArray =
+    ["January", 
+    "February", 
+    "March", 
+    "April", 
+    "May", 
+    "June", 
+    "July", 
+    "August", 
+    "September", 
+    "October", 
+    "November", 
+    "December"]
 
+    if (monthYaer === 0) return monthYaersArray[0];
+    if (monthYaer === 1)  return monthYaersArray[1];
+    if (monthYaer === 2)  return monthYaersArray[2];
+    if (monthYaer === 3)  return monthYaersArray[3];
+    if (monthYaer === 4)  return monthYaersArray[4];
+    if (monthYaer === 5)  return monthYaersArray[5];
+    if (monthYaer === 6)  return monthYaersArray[6];
+    if (monthYaer === 7)  return monthYaersArray[7];
+    if (monthYaer === 8)  return monthYaersArray[8];
+    if (monthYaer === 9)  return monthYaersArray[9];
+    if (monthYaer === 10) return monthYaersArray[10];
+    if (monthYaer === 11) return monthYaersArray[11];}
+ 
+
+function diaDaSemana() {
+
+    let diaSemana = 
+       ["Sunday",
+        "Monday", 
+        "Tuesday", 
+        "Wednesday", 
+        "Thursday", 
+        "Friday", 
+        "Saturday"];
+
+    if (day === 0) return diaSemana[0];
+    if (day === 1) return diaSemana[1];
+    if (day === 2) return diaSemana[2];
+    if (day === 3) return diaSemana[2];
+    if (day === 4) return diaSemana[2];
+    if (day === 5) return diaSemana[2];
+    if (day === 6) return diaSemana[2];
 }
 
-function diaDaSemana(){
+function mostrarNatela() {
+    let dataFormatada = diaDaSemana();
+    let dataFormatadaDia = diaDoMes()
 
-let diaSemana = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-
-if (day === 0 ) return diaSemana[0]
-if (day === 1 ) return diaSemana[1]}
-
-function mostrarNatela(){
- let dataFormatada = diaDaSemana()
-
- let resposta = document.querySelector("#res");
- resposta.innerHTML = `${dataFormatada}, ${monthDay} de`}
+    let resposta = document.querySelector("#res");
+    resposta.innerHTML = `${dataFormatada}, ${dataFormatadaDia} ${monthDay}, ${years}  `
+}
 
 mostrarNatela()
