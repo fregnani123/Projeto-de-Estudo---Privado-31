c0bba3b26c1f826d2d74f8429b2e5a59eb014d93
fregnani123 <fabianofregnani@hotmail.com>
2023-01-09 14:24:44 -0300

    Estudando

--- script.js
+++ script.js
@@ -1,69 +1,89 @@
 
+// for (let i = 0; i<= 10; i++){
+//     console.log(`Imprimir i ${i}`)
+// }
+
+let pessoas = [{nome:"Fernada"},{nome:"Fabiano"},{nome:"Fabricio"},{nome:"Ada"},{nome:"Fran"},{nome:"Bicudo"},{nome:"Maurina"},{nome:"Alline"},{nome:"jane"}]
 
 
-let data = new Date();
-let day = data.getDay();
-let monthYaer = data.getMonth();
-let years = data.getFullYear();
-let monthDay = data.getDate();
-console.log(monthDay);
+pessoas.forEach((todasAsPessoas,i,array)=> {
+    let nomes = todasAsPessoas.nome 
+    console.log(nomes,i)
+} )
 
-function MesdoAno() {
 
-    monthYaersArray =
-    ["January", 
-    "February", 
-    "March", 
-    "April", 
-    "May", 
-    "June", 
-    "July", 
-    "August", 
-    "September", 
-    "October", 
-    "November", 
-    "December"]
 
-    if (monthYaer === 0)  return monthYaersArray[0];
-    if (monthYaer === 1)  return monthYaersArray[1];
-    if (monthYaer === 2)  return monthYaersArray[2];
-    if (monthYaer === 3)  return monthYaersArray[3];
-    if (monthYaer === 4)  return monthDaydiaMes [4];
-    if (monthYaer === 5)  return monthDaydiaMes [5];
-    if (monthYaer === 6)  return monthDaydiaMes [6];
-    if (monthYaer === 7)  return monthDaydiaMes [7];
-    if (monthYaer === 8)  return monthDaydiaMes [8];
-    if (monthYaer === 9)  return monthDaydiaMes [9];
-    if (monthYaer === 10) return monthDaydiaMes [10];
-    if (monthYaer === 11) return monthDaydiaMes [11];}
+
+
+
+
+
+
+
+// //data do ano
+// let data = new Date();
+// let day = data.getDay();
+// let monthYaer = data.getMonth();
+// let years = data.getFullYear();
+// let monthDay = data.getDate();
+// console.log(monthDay);
+
+// function MesdoAno() {
+
+//     monthYaersArray =
+//     ["January", 
+//     "February", 
+//     "March", 
+//     "April", 
+//     "May", 
+//     "June", 
+//     "July", 
+//     "August", 
+//     "September", 
+//     "October", 
+//     "November", 
+//     "December"]
+
+//     if (monthYaer === 0)  return monthYaersArray[0];
+//     if (monthYaer === 1)  return monthYaersArray[1];
+//     if (monthYaer === 2)  return monthYaersArray[2];
+//     if (monthYaer === 3)  return monthYaersArray[3];
+//     if (monthYaer === 4)  return monthDaydiaMes [4];
+//     if (monthYaer === 5)  return monthDaydiaMes [5];
+//     if (monthYaer === 6)  return monthDaydiaMes [6];
+//     if (monthYaer === 7)  return monthDaydiaMes [7];
+//     if (monthYaer === 8)  return monthDaydiaMes [8];
+//     if (monthYaer === 9)  return monthDaydiaMes [9];
+//     if (monthYaer === 10) return monthDaydiaMes [10];
+//     if (monthYaer === 11) return monthDaydiaMes [11];}
  
 
-function diaDaSemana() {
+// function diaDaSemana() {
 
-    let diaSemana = 
-       ["Sunday",
-        "Monday", 
-        "Tuesday", 
-        "Wednesday", 
-        "Thursday", 
-        "Friday", 
-        "Saturday"];
+//     let diaSemana = 
+//        ["Sunday",
+//         "Monday", 
+//         "Tuesday", 
+//         "Wednesday", 
+//         "Thursday", 
+//         "Friday", 
+//         "Saturday"];
 
-    if (day === 0) return diaSemana[0];
-    if (day === 1) return diaSemana[1];
-    if (day === 2) return diaSemana[2];
-    if (day === 3) return diaSemana[2];
-    if (day === 4) return diaSemana[2];
-    if (day === 5) return diaSemana[2];
-    if (day === 6) return diaSemana[2];
-}
+//     if (day === 0) return diaSemana[0];
+//     if (day === 1) return diaSemana[1];
+//     if (day === 2) return diaSemana[2];
+//     if (day === 3) return diaSemana[2];
+//     if (day === 4) return diaSemana[2];
+//     if (day === 5) return diaSemana[2];
+//     if (day === 6) return diaSemana[2];
+// }
 
-function mostrarNatela() {
-    let dataFormatada = diaDaSemana();
-    let dataFormatadaDia = MesdoAno()
+// function mostrarNatela() {
+//     let dataFormatada = diaDaSemana();
+//     let dataFormatadaDia = MesdoAno()
 
-    let resposta = document.querySelector("#res");
-    resposta.innerHTML = `${dataFormatada}, ${dataFormatadaDia} ${monthDay}, ${years}  `
-}
+//     let resposta = document.querySelector("#res");
+//     resposta.innerHTML = `${dataFormatada}, ${dataFormatadaDia} ${monthDay}, ${years}  `
+// }
 
-mostrarNatela()
+// mostrarNatela()
