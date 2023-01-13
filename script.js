d7d4d3f73ffdd754ee117d43eb8cd66969bf3dad
fregnani123 <fabianofregnani@hotmail.com>
2023-01-12 21:33:28 -0300

    Estudando

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 
 
 
+function fb(numero){
+    if (typeof numero !== "number"){
+        return numero;
+    }
+    if (numero % 3 === 0 && numero % 5 === 0 ) { return "FizzBuzz"}
+    if (numero % 3 === 0) return "Fizz";
+    if (numero % 5 === 0) return "Buzz";
+   
+}
+for ( let i = 0 ; i <= 100; i++){
+
+    console.log(i, fb(i))
+}
+
 
 // const conteudo = {
 //     tag: "p", texto:"Qualquer texto que você queira"}
