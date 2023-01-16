c5b696018b9da9523e242c1193deb554aee08fd7
fregnani123 <fabianofregnani@hotmail.com>
2023-01-16 17:03:25 -0300

    atualizar estudos

--- script.js
+++ script.js
@@ -1,93 +1,75 @@
-// const conteudo = {
-//     tag: "p", texto:"Qualquer texto que você queira"}
+const inputTarefa = document.querySelector('.input-tarefa')
+const btnTarefa = document.querySelector('.btn-tarefa')
+const tarefas = document.querySelector('.tarefas')
 
-//   for (let chave in conteudo) {
-//     console.log(chave, conteudo[chave])
-//   }
-    
-//   const conteiner = document.querySelector(".conteiner");
-//   const div = document.createElement("div");
-    
-//   for (let i = 0; i < conteudo.length; i++) {
-//     let {tag,texto} = conteudo[i];
+function criaLi(){
+    const li = document.createElement("li")
+    return li;
+}
+inputTarefa.addEventListener("keypress", function(e) {
+    if (e.keyCode === 13){
+        if (!inputTarefa.value) return
+        criaTarefa(inputTarefa.value);
+    }
+})
 
-//  const tagCriada = document.createElement(tag);
-//  tagCriada.innerText = texto;
-//  div.appendChild(tagCriada)
- 
-// }
-  
-// conteiner.appendChild(div)
+function limpaInput(){
+    inputTarefa.value = "";
+    inputTarefa.focus();
+}
 
+function criaBotaoApagar(li){
+ li.innerText += " ";
+ const botaoApagar = document.createElement("button");
+ botaoApagar.innerHTML = "Apagar";
+ botaoApagar.setAttribute("class", 'apagar')
+ li.appendChild(botaoApagar);
+}
 
+function criaTarefa(textoInput){
+ const li = criaLi();
+ li.innerText = textoInput;
+ tarefas.appendChild(li);
+ limpaInput();
+ criaBotaoApagar(li);
+ salvarTarefa();
+}
 
+btnTarefa.addEventListener("click", function () {
+    if (!inputTarefa.value) return;
+  criaTarefa(inputTarefa.value);
+})
 
+document.addEventListener("click", function(e) {
+  const el = e.target;
+  if (el.classList.contains("apagar")) {
+    el.parentElement.remove();
+    salvarTarefa();
+  }
 
+});
 
-// //data do ano
-// let data = new Date();
-// let day = data.getDay();
-// let monthYaer = data.getMonth();
-// let years = data.getFullYear();
-// let monthDay = data.getDate();
-// console.log(monthDay);
+function salvarTarefa(){
+const liTarefas = tarefas.querySelectorAll("li")
+const listaDeTarefas = []
+for (let tarefa of liTarefas){
+  let tarefaTexto = tarefa.innerText; 
+  tarefaTexto = tarefaTexto.replace("apagar", "").trim();
+  listaDeTarefas.push(tarefaTexto);
+}
 
-// function MesdoAno() {
+const tarefasJson = JSON.stringify(listaDeTarefas);
 
-//     monthYaersArray =
-//     ["January", 
-//     "February", 
-//     "March", 
-//     "April", 
-//     "May", 
-//     "June", 
-//     "July", 
-//     "August", 
-//     "September", 
-//     "October", 
-//     "November", 
-//     "December"]
+localStorage.setItem("tarefas", tarefasJson );
+}
 
-//     if (monthYaer === 0)  return monthYaersArray[0];
-//     if (monthYaer === 1)  return monthYaersArray[1];
-//     if (monthYaer === 2)  return monthYaersArray[2];
-//     if (monthYaer === 3)  return monthYaersArray[3];
-//     if (monthYaer === 4)  return monthDaydiaMes [4];
-//     if (monthYaer === 5)  return monthDaydiaMes [5];
-//     if (monthYaer === 6)  return monthDaydiaMes [6];
-//     if (monthYaer === 7)  return monthDaydiaMes [7];
-//     if (monthYaer === 8)  return monthDaydiaMes [8];
-//     if (monthYaer === 9)  return monthDaydiaMes [9];
-//     if (monthYaer === 10) return monthDaydiaMes [10];
-//     if (monthYaer === 11) return monthDaydiaMes [11];}
- 
+function adicionaTarefasSalvas(){
+    const tarefas = localStorage.getItem("tarefas");
+    const listaDeTarefas = JSON.parse('tarefas');
 
-// function diaDaSemana() {
+    for (let tarefa of listaDeTarefas){
+        criaTarefa(tarefa)
+    }
+}
+adicionaTarefasSalvas()
 
-//     let diaSemana = 
-//        ["Sunday",
-//         "Monday", 
-//         "Tuesday", 
-//         "Wednesday", 
-//         "Thursday", 
-//         "Friday", 
-//         "Saturday"];
-
-//     if (day === 0) return diaSemana[0];
-//     if (day === 1) return diaSemana[1];
-//     if (day === 2) return diaSemana[2];
-//     if (day === 3) return diaSemana[2];
-//     if (day === 4) return diaSemana[2];
-//     if (day === 5) return diaSemana[2];
-//     if (day === 6) return diaSemana[2];
-// }
-
-// function mostrarNatela() {
-//     let dataFormatada = diaDaSemana();
-//     let dataFormatadaDia = MesdoAno()
-
-//     let resposta = document.querySelector("#res");
-//     resposta.innerHTML = `${dataFormatada}, ${dataFormatadaDia} ${monthDay}, ${years}  `
-// }
-
-// mostrarNatela()
