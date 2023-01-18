5cc894661dd4f1e3995d1c4c239de0fd79fcd3d5
fregnani123 <fabianofregnani@hotmail.com>
2023-01-18 20:55:14 -0300

    estudando

--- script.js
+++ script.js
@@ -1,13 +1,57 @@
+const inputTarefa = document.querySelector(".input-tarefa")
+const btnInputTarefa = document.querySelector('.btn-tarefa')
+const tarefas = document.querySelector('.tarefas')
 
-let n= 3 
-
-let sorteio = function (){
-let sortear = (Math.round(Math.random() * n))
-console.log(++sortear)
+function criaLi(){
+    const li = document.createElement("li");
+    return li;
 }
 
-// // document.addEventListener('click', function(e) {
-//    let el = e.target
-//    console.log(el)
-//    return
-// })
+inputTarefa.addEventListener("keypress", function(e) {
+   if (e.keyCode === 13){
+    if (!inputTarefa.value) return;
+    criaTarefa(inputTarefa.value)
+   }
+   
+})
+
+function limpaInput(){
+    inputTarefa.value = ""
+    inputTarefa.focus();
+}
+
+function criaTarefa(textoInput){
+  const li = criaLi();
+ li.innerText = textoInput
+ tarefas.appendChild(li);
+ limpaInput()
+ criaBotaoApagar(li)
+ salvarTarefas()
+}
+
+btnInputTarefa.addEventListener("click", function () {
+ if (!inputTarefa.value) return;
+ criaTarefa(inputTarefa.value)
+}
+)
+
+function criaBotaoApagar(li){
+    li.innerText += " ";
+    const botaoApagar = document.createElement("button");
+    botaoApagar.innerText= "Apagar";
+    botaoApagar.setAttribute("class", "apagar");
+    botaoApagar.setAttribute("title", "Excluir tarefa")
+    li.appendChild(botaoApagar);
+    salvarTarefas()
+}
+
+document.addEventListener("click", function(e) {
+    const el = e.target;
+    if (el.classList.contains("apagar")){
+        el.parentElement.remove()
+    }
+})
+
+function salvarTarefas(){
+    const liTarefas = tarefas.querySelectorAll("li");
+}
