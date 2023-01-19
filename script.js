6b2b5757f2a8f6a45827808ff407c02ab0d5aacb
fregnani123 <fabianofregnani@hotmail.com>
2023-01-19 10:19:48 -0300

    Estudos a todo vapor

--- script.js
+++ script.js
@@ -1,57 +1,6 @@
-const inputTarefa = document.querySelector(".input-tarefa")
-const btnInputTarefa = document.querySelector('.btn-tarefa')
-const tarefas = document.querySelector('.tarefas')
 
-function criaLi(){
-    const li = document.createElement("li");
-    return li;
-}
+let lista = ['t1','t2','t3','t4']
+let listaJSON = JSON.stringify(lista);
+let listaSalva = JSON.parse(listaJSON)
 
-inputTarefa.addEventListener("keypress", function(e) {
-   if (e.keyCode === 13){
-    if (!inputTarefa.value) return;
-    criaTarefa(inputTarefa.value)
-   }
-   
-})
-
-function limpaInput(){
-    inputTarefa.value = ""
-    inputTarefa.focus();
-}
-
-function criaTarefa(textoInput){
-  const li = criaLi();
- li.innerText = textoInput
- tarefas.appendChild(li);
- limpaInput()
- criaBotaoApagar(li)
- salvarTarefas()
-}
-
-btnInputTarefa.addEventListener("click", function () {
- if (!inputTarefa.value) return;
- criaTarefa(inputTarefa.value)
-}
-)
-
-function criaBotaoApagar(li){
-    li.innerText += " ";
-    const botaoApagar = document.createElement("button");
-    botaoApagar.innerText= "Apagar";
-    botaoApagar.setAttribute("class", "apagar");
-    botaoApagar.setAttribute("title", "Excluir tarefa")
-    li.appendChild(botaoApagar);
-    salvarTarefas()
-}
-
-document.addEventListener("click", function(e) {
-    const el = e.target;
-    if (el.classList.contains("apagar")){
-        el.parentElement.remove()
-    }
-})
-
-function salvarTarefas(){
-    const liTarefas = tarefas.querySelectorAll("li");
-}
+console.log(listaSalva)
