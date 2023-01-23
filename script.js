61506fd510abd7f7c0203bee8c4e806bd1d35086
fregnani123 <fabianofregnani@hotmail.com>
2023-01-23 16:58:48 -0300

    atualiza estudos

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
 function Calculadora() {
     this.display = document.querySelector('.display');
 
+    this.inicia = () => this.copturaCliques();
+
     this.copturaCliques = () => {
         document.addEventListener('click', e => {
             const el = e.target;
             if (el.classList.contains("btn-num")) this.addNumDisplay(el);
             if (el.classList.contains("btn-clear")) this.clear();
             if (el.classList.contains("btn-del")) this.del(el);
-            if (el.classList.contains("btn-eq")) this.realizaConta(el);
+            if (el.classList.contains("btn-eq")) this.realizaConta();
         });
     };
 
+ this.realizaConta = () => {
+ try{
+  const conta = eval(this.display.value);
+  if(!conta){alert("conta invalida")
+return};
+  this.display.value = conta
+ }catch(e){
+   alert("conta invalida")
+   return;
+ }};
+
   this.addNumDisplay = el => this.display.value += el.innerText;
   this.clear = () => this.display.value = ""
-  this.inicia = () => this.copturaCliques();
-  this.del = (e) => this
-
+  this.del = () => this.display.value = this.display.value.slice(0, -1);
+ 
   }
-
 
 const calculadora = new Calculadora();
 calculadora.inicia();
