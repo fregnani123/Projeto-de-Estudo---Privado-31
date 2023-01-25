d476f988690d2eb45fbff01fe785b9ffbc4a348c
fregnani123 <fabianofregnani@hotmail.com>
2023-01-25 15:02:03 -0300

    estudar

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ btnMobile.addEventListener("click", (e) => {
   menuMobile.classList.toggle("active")
 })
 
-
 function Calculadora(){
  this.display = document.querySelector(".display");
 
-
 this.capturaCliques = () => {
   document.addEventListener("click", e => { const el = e.target;
-    
-  })
-}
-
+    if (el.classList.contains("btn-num")) this.addNumDisplay(el);
+  });
+};
+this.addNumDisplay = (el) => {
+ this.display.value += el.innerText;
+};
  this.inicia = () => {
    this.capturaCliques();
  }
@@ -25,8 +25,6 @@ this.capturaCliques = () => {
 
 const calculadora = new Calculadora();
 calculadora.inicia();
-
-
 
 
 
