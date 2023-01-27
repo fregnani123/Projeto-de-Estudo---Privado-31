2e44187d0c19e793665efc1f8a920564d9da8f72
fregnani123 <fabianofregnani@hotmail.com>
2023-01-27 20:28:05 -0300

    estudos

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
-const btnMobile = document.querySelector(".btn-mobile");
-const menuMobile = document.querySelector(".nav");
+// const btnMobile = document.querySelector(".btn-mobile");
+// const menuMobile = document.querySelector(".nav");
 
-btnMobile.addEventListener("click", (e) => {
-  e.preventDefault();
-  menuMobile.classList.toggle("active")
-})
+// btnMobile.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   menuMobile.classList.toggle("active")
+// })
 
 
 const inputTarefa = document.querySelector(".input-tarefa");
@@ -20,8 +20,11 @@ function criaLi(){
 function criaBotaoApagar(li){
   li.innerText += " ";
 const botaoApagar = document.createElement('button');
+botaoApagar.setAttribute('class','apagar')
 botaoApagar.innerText = 'Apagar';
 li.appendChild(botaoApagar);
+
+
 };
 
 inputTarefa.addEventListener('keypress', e => {
@@ -41,6 +44,8 @@ function criaTarefa(textoInput){
  li.innerText = textoInput;
  tarefas.appendChild(li);
  limpaInput();
+ criaBotaoApagar(li);
+ salvarTarefas();
 }
 
 btnTarefa.addEventListener('click', () =>{
@@ -48,4 +53,33 @@ if(!inputTarefa.value)return;
 criaTarefa(inputTarefa.value);
 })
 
+document.addEventListener('click', e => {
+  const el = e.target;
+  if(el.classList.contains('apagar')){
+     el.parentElement.remove();
+     salvarTarefas();
+  }
+})
+
+function salvarTarefas(){
+ const liTarefas = tarefas.querySelectorAll('li');
+ const listadeTarefas = [];
+ for (let tarefa of liTarefas){
+ let tarefaTexto = tarefa.innerText;
+ tarefaTexto = tarefaTexto.replace('Apagar',"").trim()
+listadeTarefas.push(tarefaTexto);
+
+ }
+const tarefaJSON = JSON.stringify(listadeTarefas);
+
+localStorage.setItem('tarefas', tarefaJSON)
+}
+function addTarefasSalvas(){
+  const tarefas = localStorage.getItem('tarefas'); 
+ const listaDeTarefas = JSON.parse(tarefas)
+ for (let tarefa of listaDeTarefas){
+  criaTarefa(tarefa)
+ }
+}
+addTarefasSalvas()
 
