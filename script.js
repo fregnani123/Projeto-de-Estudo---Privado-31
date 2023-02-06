8e9f65089f2c27b5a153e43f9e792ec5966e7850
fregnani123 <fabianofregnani@hotmail.com>
2023-02-06 15:38:04 -0300

    estudar

--- script.js
+++ script.js
@@ -2,28 +2,48 @@
 function NovaPagina(){
 
    this.divPai = document.querySelector(".container");
-
+   
   this.iniciar = () => {
-  this.addDiv1()
+  this.addHeader()
   }
 
-  this.criaDiv = function(){
-    let div = document.createElement("div")
-   return div; 
-  }
+let optionsMenu = ["About","Portfolio","Contact"]
 
- this.addDiv1 = () => {
-  let div1 = this.criaDiv()
+ this.addHeader = () => {
+  let div1 = this.criaDiv();
+  let ul = this.criaUl();
+  let li = this.criaLI();
+  let menu = this.menu();
   div1.setAttribute("class","header");
- 
-  this.divPai.appendChild(div1)
+  this.divPai.appendChild(div1);
+   div1.appendChild(ul);
+   ul.appendChild(li);
+   li.innerText += menu;
  };
+
+ this.menu = (li = optionsMenu) => {
+  for (let i = 0; i < li.length; i++) {
+  console.log(li[i]);
+} 
+ }
+  
+ this.criaDiv = function(){
+  let div = document.createElement("div")
+ return div; 
+};
+ this.criaUl = () => {
+  let ul = document.createElement("ul")
+  return ul
+};
+
+ this.criaLI = () => {
+  let li = document.createElement("li");
+  return li;
+ };
+
  
   }
-  
 
 let novaPagina = new NovaPagina();
 
-novaPagina.iniciar()
-
-
+novaPagina.iniciar();
