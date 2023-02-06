95839524e24dc34627c07f65ea8cff5c71af65d0
fregnani123 <fabianofregnani@hotmail.com>
2023-02-06 17:10:51 -0300

    estudos

--- script.js
+++ script.js
@@ -1,49 +1,35 @@
 
 function NovaPagina(){
+   this.menu = ["About","Portfolio","Contact",];
 
-   this.divPai = document.querySelector(".container");
+   this.container = document.querySelector(".container");
    
   this.iniciar = () => {
   this.addHeader()
   }
 
-let optionsMenu = ["About","Portfolio","Contact"]
+ this.addHeader = function() {
+  let header = this.criaDiv();
+  let menu = document.createElement("ul");
+  header.setAttribute("class","header");
+  this.container.appendChild(header);
+  header.appendChild(menu);
 
- this.addHeader = () => {
-  let div1 = this.criaDiv();
-  let ul = this.criaUl();
-  let li = this.criaLI();
-  let menu = this.menu();
-  div1.setAttribute("class","header");
-  this.divPai.appendChild(div1);
-   div1.appendChild(ul);
-   ul.appendChild(li);
-   li.innerText += menu;
- };
-
- this.menu = (li = optionsMenu) => {
-  for (let i = 0; i < li.length; i++) {
-  console.log(li[i]);
-} 
- }
-  
+  for (let i = 0; i < this.menu.length; i++){
+     let listaMenu = document.createElement("li")
+     listaMenu.innerText = this.menu[i]
+     menu.appendChild(listaMenu)
+  }}
+          
  this.criaDiv = function(){
   let div = document.createElement("div")
  return div; 
 };
- this.criaUl = () => {
-  let ul = document.createElement("ul")
-  return ul
-};
 
- this.criaLI = () => {
-  let li = document.createElement("li");
-  return li;
- };
-
- 
   }
 
 let novaPagina = new NovaPagina();
 
 novaPagina.iniciar();
+
+
