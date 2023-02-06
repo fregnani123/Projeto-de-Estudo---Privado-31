421ff3e0a2d0fa967d0aaa8faa5672017d9ccb96
fregnani123 <fabianofregnani@hotmail.com>
2023-02-06 19:07:02 -0300

    atualizar

--- script.js
+++ script.js
@@ -1,34 +1,51 @@
 
 function NovaPagina(){
+
    this.menu = ["About","Portfolio","Contact",];
+   this.colorListaMenu = "white";
+   this.colorMenu = "black"
 
    this.container = document.querySelector(".container");
    
   this.iniciar = () => {
   this.addHeader()
+  this.criaUl()
   }
 
  this.addHeader = function() {
   let header = this.criaDiv();
-  let menu = document.createElement("ul");
+  let ul = this.criaUl()
   header.setAttribute("class","header");
   this.container.appendChild(header);
-  header.appendChild(menu);
+  header.appendChild(ul);
 
   for (let i = 0; i < this.menu.length; i++){
      let listaMenu = document.createElement("li")
-     listaMenu.innerText = this.menu[i]
-     menu.appendChild(listaMenu)
-  }}
-          
+     listaMenu.innerHTML= this.menu[i]
+     listaMenu.style.color = this.colorListaMenu;
+     ul.appendChild(listaMenu)
+  }
+}
+
+this.criaUl = function (){
+  let ul = document.createElement("ul");
+  ul.style.display = "flex";
+  ul.style.gap = "20px";
+  ul.style.listStyle = "none";
+  ul.style.fontSize = "20px"
+  return ul
+}
+      
  this.criaDiv = function(){
-  let div = document.createElement("div")
+  let div = document.createElement("div");
+  div.style.background = this.colorMenu
  return div; 
 };
 
+
   }
 
-let novaPagina = new NovaPagina();
+const novaPagina = new NovaPagina();
 
 novaPagina.iniciar();
 
