ec9a45d48168765901a1878035413ed499609b4d
fregnani123 <fabianofregnani@hotmail.com>
2023-02-06 14:11:43 -0300

    atulizar estudos

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 
 function NovaPagina(){
+
    this.divPai = document.querySelector(".container");
-   
-   this.iniciar = () => {
-    let div2 = this.criaDiv()
-    div2.setAttribute("class","novaDiv");
-    this.divPai.appendChild(div2)
+
+  this.iniciar = () => {
+  this.addDiv1()
   }
 
-   this.criaDiv = function(){
-     let div = document.createElement("div")
-    return div; 
-   }
+  this.criaDiv = function(){
+    let div = document.createElement("div")
+   return div; 
+  }
 
-
-  } 
-
+ this.addDiv1 = () => {
+  let div1 = this.criaDiv()
+  div1.setAttribute("class","header");
+ 
+  this.divPai.appendChild(div1)
+ };
+ 
+  }
+  
 
 let novaPagina = new NovaPagina();
 
