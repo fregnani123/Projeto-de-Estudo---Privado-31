54697eb02eb350b03eb278f0d71c8c919017143b
fregnani123 <fabianofregnani@hotmail.com>
2023-02-08 16:21:38 -0300

    estudos

--- script.js
+++ script.js
@@ -1,13 +1,2 @@
-
-const btnMobile = document.querySelector('#btn-mobile')
-
-
-function toggleMenu(event){
-  if (event.type === 'touchstart') event.preventDefault()
-  const nav = document.querySelector('#nav');
-  nav.classList.toggle('active')}
-
-btnMobile.addEventListener('click', toggleMenu);
-btnMobile.addEventListener('touchstart', toggleMenu);
-
+//684393151f44eea088d129fd08a67dee
 
