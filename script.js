51e015941b124fa1001b7b6f403424d3546c1aa3
fregnani123 <fabianofregnani@hotmail.com>
2023-02-10 17:23:25 -0300

    estudos

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 
+//caneta, camisa
 
+function Produto(nome,preco,cor){
+ this.nome = nome;
+ this.preco = preco;
+ this.cor= cor;
+}
+Produto.prototype.aumento = function(quantia){
+    this.preco += quantia;
+}
+Produto.prototype.desconto = function(quantia){
+    this.preco -= quantia;
+}
 
+function Camiseta(nome, preco,cor){
+    Produto.call(this, nome, preco,cor);
+}
 
+const camiseta = new Camiseta("regata", 25.50, "preta")
 
-
-
-
-
+console.log(camiseta)
 
 
 
