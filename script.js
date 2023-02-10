de2bcab8ba0ccec4370621c066aea6d821c81661
fregnani123 <fabianofregnani@hotmail.com>
2023-02-10 18:38:20 -0300

    estudos

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
 
 //caneta, camisa
 
-function Produto(nome,preco,cor){
+function Produto(nome,preco){
  this.nome = nome;
  this.preco = preco;
- this.cor= cor;
 }
 Produto.prototype.aumento = function(quantia){
     this.preco += quantia;
 }
 Produto.prototype.desconto = function(quantia){
-    this.preco -= quantia;
+    this.preco -= (this.preco * (quantia / 100));
 }
 
 function Camiseta(nome, preco,cor){
+    this.cor = cor
     Produto.call(this, nome, preco,cor);
 }
 
-const camiseta = new Camiseta("regata", 25.50, "preta")
+Camiseta.prototype = Object.create(Produto.prototype);
+Camiseta.prototype.constructor = Camiseta;
 
+Camiseta.prototype.desconto = function(quantia) {
+    this.preco -= (this.preco * (quantia / 100));
+}
+
+function Caneca (nome, preco, material){
+    this.material = material;
+    Produto.call(this, nome, preco, material );
+}
+
+Caneca.prototype = Object.create(Produto.prototype);
+
+
+
+const camiseta = new Camiseta("regata", 25.50, "preta");
+
+const caneca = new Caneca ("caneca", 80.50, "aluminio")
+
+camiseta.desconto(10)
 console.log(camiseta)
+console.log(caneca)
 
 
 
