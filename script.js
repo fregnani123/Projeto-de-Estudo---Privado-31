c4d1c91c6728e84a1115bac610d2ee16a260f874
fregnani123 <fabianofregnani@hotmail.com>
2023-02-21 21:39:31 -0300

    studing

--- script.js
+++ script.js
@@ -1,45 +1,61 @@
-function ValidaCPF(cpfEnviado){
-  Object.defineProperty(this, 'cpfLimpo', {
-    enumerable: true,
-    get: function (){
-        return cpfEnviado.replace(/\D+/g, '');
-    }
-  })
-}
-ValidaCPF.prototype.valida = function(){
-if(typeof this.cpfLimpo === 'undefined')return false;
-if(this.cpfLimpo.length !== 11) return false;
-if (this.isSequencia()) return false;
-const cpfParcial = this.cpfLimpo.slice(0, -2)
-const digito1 = this.criaDigito(cpfParcial);
-const digito2 = this.criaDigito(cpfParcial + digito1);
 
-const novoCpf = cpfParcial + digito1 + digito2;
 
-return novoCpf === this.cpfLimpo;
-}
+let juntar = ("063.912.989-71")
+let semjuntar = juntar.replace(/\D+/g, '');
 
-ValidaCPF.prototype.isSequencia = function () {
-    const sequencia =  this.cpfLimpo[0].repeat(this.cpfLimpo.length);
-    return sequencia === this.cpfLimpo;
-}
+let array = Array.from(semjuntar)
 
- ValidaCPF.prototype.criaDigito = function(cpfParcial){
-let cpfArray = Array.from(cpfParcial);
+let novoArray = array.slice(0, -2)
 
-let regressivo = cpfArray.length + 1;
-let total = cpfArray.reduce((ac, val)=>{
-    ac += (regressivo * Number(val))
-    regressivo--;
-    return ac;
-},0)
-const digito = 11 - (total % 11)
-return digito > 9 ? '0' : String(digito);
-}
+console.log(novoArray)
 
-let inputCPF = document.querySelector(".cpf")
-const cpf = new ValidaCPF(inputCPF.value);
-console.log(cpf.valida());
+
+
+
+
+
+// function ValidaCPF(cpfEnviado){
+//   Object.defineProperty(this, 'cpfLimpo', {
+//     enumerable: true,
+//     get: function (){
+//         return cpfEnviado.replace(/\D+/g, '');
+//     }
+//   })
+// }
+// ValidaCPF.prototype.valida = function(){
+// if(typeof this.cpfLimpo === 'undefined')return false;
+// if(this.cpfLimpo.length !== 11) return false;
+// if (this.isSequencia()) return false;
+// const cpfParcial = this.cpfLimpo.slice(0, -2)
+// const digito1 = this.criaDigito(cpfParcial);
+// const digito2 = this.criaDigito(cpfParcial + digito1);
+
+// const novoCpf = cpfParcial + digito1 + digito2;
+
+// return novoCpf === this.cpfLimpo;
+// }
+
+// ValidaCPF.prototype.isSequencia = function () {
+//     const sequencia =  this.cpfLimpo[0].repeat(this.cpfLimpo.length);
+//     return sequencia === this.cpfLimpo;
+// }
+
+//  ValidaCPF.prototype.criaDigito = function(cpfParcial){
+// let cpfArray = Array.from(cpfParcial);
+
+// let regressivo = cpfArray.length + 1;
+// let total = cpfArray.reduce((ac, val)=>{
+//     ac += (regressivo * Number(val))
+//     regressivo--;
+//     return ac;
+// },0)
+// const digito = 11 - (total % 11)
+// return digito > 9 ? '0' : String(digito);
+// }
+
+// let inputCPF = document.querySelector(".cpf")
+// const cpf = new ValidaCPF(inputCPF.value);
+// console.log(cpf.valida());
 
 
 
