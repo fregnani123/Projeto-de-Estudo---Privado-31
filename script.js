3dc56349e7d056674700aaccd505ef4bff03ba5e
fregnani123 <fabianofregnani@hotmail.com>
2023-02-24 17:41:07 -0300

    estudos

--- script.js
+++ script.js
@@ -15,19 +15,47 @@ class ValidaCPF {
     eSequencia(){
         return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo
     }
+    geraNovoCpf(){
+        const cpfSemDigitos = this.cpfLimpo.slice(0, -2);
 
+    let digito1 = this.geraDigito(cpfSemDigitos)
+    let digito2 = this.geraDigito(cpfSemDigitos + digito1)
+    this.geraNovoCpf = cpfSemDigitos + digito1 + digito2;
+
+    }
+
+    geraDigito(cpfSemDigitos){
+   let total = 0;
+   let reverso = cpfSemDigitos.length + 1;
+   for(let stringNumerica of cpfSemDigitos){
+    total += reverso * Number(stringNumerica)
+    reverso--
+   }
+   const digito = 11 - (total % 11);
+   return digito <= 9 ? String(digito) : "0";
+
+    }
+  
     valida(){
         
         if(!this.cpfLimpo) return false;
         if(typeof this.cpfLimpo !== "string") return false;
         if(this.cpfLimpo.length !== 11) return false;
-        if(this.eSequencia()) return false
-        return "cheguei aqui"
+        if(this.eSequencia()) return false;
+        this.geraNovoCpf()
+        console.log(this.geraNovoCpf)
+     return this.geraNovoCpf === this.cpfLimpo
     }
 }
 
-const validacpc = new ValidaCPF("063.912.989-71")
-console.log(validacpc.valida());
+const validacpc = new ValidaCPF("111.111.111-11")
+
+ if (validacpc.valida()){
+    console.log("CPF válido");
+ }else{
+    console.log("CPF invalido")
+ }
+
 
 
 
