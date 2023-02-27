2b861a318686adf9c4ff3b5f7caa7a53efbf06e5
fregnani123 <fabianofregnani@hotmail.com>
2023-02-27 14:12:42 -0300

    estudos

--- script.js
+++ script.js
@@ -1,60 +1,128 @@
+class ValidaCPF { 
+    constructor (cpfEnviado){
+        Object.defineProperty(this, 'cpfLimpo', {
+            value: cpfEnviado.replece(/\D+/g, '')
 
-let volume = document.querySelector(".h1");
-
-
-class ValidaCPF {
-    constructor (cpfEnviado) {
- Object.defineProperty(this, "cpfLimpo", {
-    writable: false,
-    enumerable: true,
-    configurable:false,
-    value: cpfEnviado.replace(/\D+/g, '')
- });
-    }
-    
-    eSequencia(){
-        return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo
-    }
-    geraNovoCpf(){
-        const cpfSemDigitos = this.cpfLimpo.slice(0, -2);
-
-    let digito1 = this.geraDigito(cpfSemDigitos)
-    let digito2 = this.geraDigito(cpfSemDigitos + digito1)
-    this.geraNovoCpf = cpfSemDigitos + digito1 + digito2;
-
-    }
-
-    geraDigito(cpfSemDigitos){
-   let total = 0;
-   let reverso = cpfSemDigitos.length + 1;
-   for(let stringNumerica of cpfSemDigitos){
-    total += reverso * Number(stringNumerica)
-    reverso--
-   }
-   const digito = 11 - (total % 11);
-   return digito <= 9 ? String(digito) : "0";
-
-    }
-  
-    valida(){
-        
-        if(!this.cpfLimpo) return false;
-        if(typeof this.cpfLimpo !== "string") return false;
-        if(this.cpfLimpo.length !== 11) return false;
-        if(this.eSequencia()) return false;
-        this.geraNovoCpf()
-        console.log(this.geraNovoCpf)
-     return this.geraNovoCpf === this.cpfLimpo
+        })
     }
 }
+const validacpf = new ValidaCPF('06391298971')
+console.logo(validacpf)
 
-const validacpc = new ValidaCPF("111.111.111-11")
 
- if (validacpc.valida()){
-    console.log("CPF válido");
- }else{
-    console.log("CPF invalido")
- }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Função Class verificação do cpf
+// let volume = document.querySelector(".h1");
+
+
+// class ValidaCPF {
+//     constructor (cpfEnviado) {
+//  Object.defineProperty(this, "cpfLimpo", {
+//     writable: false,
+//     enumerable: true,
+//     configurable:false,
+//     value: cpfEnviado.replace(/\D+/g, '')
+//  });
+//     }
+    
+//     eSequencia(){
+//         return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo
+//     }
+//     geraNovoCpf(){
+//         const cpfSemDigitos = this.cpfLimpo.slice(0, -2);
+
+//     let digito1 = ValidaCPF.geraDigito(cpfSemDigitos)
+//     let digito2 = ValidaCPF.geraDigito(cpfSemDigitos + digito1)
+//     this.geraNovoCpf = cpfSemDigitos + digito1 + digito2;
+
+//     }
+
+//    static geraDigito(cpfSemDigitos){
+//    let total = 0;
+//    let reverso = cpfSemDigitos.length + 1;
+//    for(let stringNumerica of cpfSemDigitos){
+//     total += reverso * Number(stringNumerica)
+//     reverso--
+//    }
+//    const digito = 11 - (total % 11);
+//    return digito <= 9 ? String(digito) : "0";
+//     }
+//     valida(){
+        
+//         if(!this.cpfLimpo) return false;
+//         if(typeof this.cpfLimpo !== "string") return false;
+//         if(this.cpfLimpo.length !== 11) return false;
+//         if(this.eSequencia()) return false;
+//         this.geraNovoCpf()
+//         console.log(this.geraNovoCpf)
+//      return this.geraNovoCpf === this.cpfLimpo
+//     }
+// }
+
+// const validacpc = new ValidaCPF("063.912.989-71")
+
+//  if (validacpc.valida()){
+//     console.log("CPF válido");
+//  }else{
+//     console.log("CPF invalido")
+//  }
 
 
 
