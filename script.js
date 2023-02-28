d5cb1cce47fa7e4ef2cc9c044321453e7d9e34e4
fregnani123 <fabianofregnani@hotmail.com>
2023-02-28 17:12:55 -0300

    estudos

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 
 
+function rand(max, min){
+   max *= 1000;
+   min *= 1000;
+   
+   return Math.floor(Math.random() * (max - min) + min);
+}
 
+function esperaAi(msg, tempo,) {
+   return new Promise((resolve, reject) => {  setTimeout(() => {
+      resolve(msg);
+   }, tempo);})
+}
 
-
-
-
-
-
-
+esperaAi("frase 1", rand(1,3)).then(res =>{
+   console.log(res);
+   return esperaAi("frase 2", rand(1,3));
+}).then((res => {
+    console.log(res)
+})).catch()
+   
 
 
 
