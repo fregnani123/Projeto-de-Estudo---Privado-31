91e8709713216586b1fae85d01b2f08a0efe5b8a
fregnani123 <fabianofregnani@hotmail.com>
2023-04-03 14:57:18 -0300

    estudos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,49 @@
-axios('pessoas.json').then(
-  res => carregaElementos(res.data));
 
-  function carregaElementos(json){
-    const table = document.createElement("table");
-    for(let pessoa of json){
-      console.log(pessoa.nome)
-      const tr = document.createElement("tr");
-      const td = document.createElement("td");
-      td.innerHTML = pessoa.nome;
-      tr.appendChild(td);
 
-      table.appendChild(tr);
 
-    }
-    const resultado = document.querySelector(".resultado");
-    resultado.appendChild(table)
-  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// axios('pessoas.json').then(
+//   res => carregaElementos(res.data));
+
+//   function carregaElementos(json){
+//     const table = document.createElement("table");
+//     for(let pessoa of json){
+//       console.log(pessoa.nome)
+//       const tr = document.createElement("tr");
+//       const td = document.createElement("td");
+//       td.innerHTML = pessoa.nome;
+//       tr.appendChild(td);
+
+//       table.appendChild(tr);
+
+//     }
+//     const resultado = document.querySelector(".resultado");
+//     resultado.appendChild(table)
+//   }
