3784a3c5454245d16b1f4f27ebd18454a41a78f0
fregnani123 <fabianofregnani@hotmail.com>
2023-04-03 17:21:55 -0300

    estudos

--- estudos.js
+++ estudos.js
@@ -1,9 +1,20 @@
 
+
+
+
+
 let n = [15,55,78,1,5,8,9,44,52,12]
-let reverso = n.length - 9; 
-let total = 0;
-for (numeros of n){
-    total += numeros * reverso
-    console.log(numeros + "X " + reverso,numeros * reverso, total)
-   ++reverso
-}
+
+let resultado = n.reduce((a,e)=> {
+    return( a + e ) 
+},0)
+console.log(resultado)
+
+// let n = [15,55,78,1,5,8,9,44,52,12]
+// let reverso = n.length - 9; 
+// let total = 0;
+// for (numeros of n){
+//     total += numeros * reverso
+//     console.log(numeros + "X " + reverso,numeros * reverso, total)
+//    ++reverso
+// }
